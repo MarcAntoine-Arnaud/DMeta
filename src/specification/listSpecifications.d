ba805ee202267c31d6c4d00acee790b6d4fc1a2f
marco <arnaud.marcantoine@gmail.com>
2014-08-22 20:13:47 +0200

    add a class to list and retrieve specifications

---
+++ src/specification/listSpecifications.d
@@ -0,0 +1,45 @@
+import specification.spec;
+import specification.formatSpecifications.wav;
+import specification.formatSpecifications.mxf;
+
+class ListSpecifications {
+	this( bool preload = false ){
+		if( preload )
+			load();
+	}
+
+	void load(){
+		auto wavSpec = new WavSpecification();
+		auto mxfSpec = new MxfSpecification();
+		add( wavSpec );
+		add( mxfSpec );
+	}
+
+	bool contains( string id ){
+		foreach( Specification s; _specList ){
+			if( s._id == id )
+				return true;
+		}
+		return false;
+	}
+
+	Specification get( string id ){
+		foreach( Specification s; _specList ){
+			if( s._id == id )
+				return s;
+		}
+		assert(0);
+	}
+
+	Specification[] getAllSpecs(){
+		return _specList;
+	}
+
+private:
+	void add( Specification spec ){
+		_specList.length = _specList.length + 1;
+		_specList[_specList.length - 1] = spec;
+	}
+
+	Specification[] _specList;
+}

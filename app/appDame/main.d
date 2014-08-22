660024da21751b9110059f7d4495b6a9354f41a6
marco <arnaud.marcantoine@gmail.com>
2014-08-22 20:14:27 +0200

    updat application to select and list specifications

--- app/appDame/main.d
+++ app/appDame/main.d
@@ -1,29 +1,29 @@
-import specification.nodes.nodeChar;
-import specification.nodes.nodeShort;
-import specification.nodes.nodeInt;
-import specification.node;
 import specification.spec;
-import specification.formatSpecifications.wav;
-import specification.formatSpecifications.mxf;
+import specification.listSpecifications;
 import comparator.comp;
 import std.stdio;
 import std.getopt;
 
 int majorVersion = 0;
 int minorVersion = 0;
+int releaseVersion = 0;
 
 void main(string[] args) {
 
 	enum Color { no, yes };
 
 	string filename = "";
+	string spec = "";
 	Color color;
+	bool specs = false;
 	bool verbose = false;
 	bool help = false;
 
 	getopt(
 		args,
 		"file|f", &filename,
+		"spec|s", &spec,
+		"specs", &specs,
 		"verbose|v", &verbose,
 		"color|c", &color,
 		"help|h", &help,
@@ -31,26 +31,43 @@ void main(string[] args) {
 
 
 	if( help ) {
-		writeln( "dame - ", majorVersion, ".", minorVersion );
+		writeln( "dame - ", majorVersion, ".", minorVersion, ".", releaseVersion );
 		writeln( "" );
-		writeln( " file: input filename" );
-		writeln( " verbose: enable verbosity" );
+		writeln( " --file -f: \tinput filename" );
+		writeln( " --spec -s: \tspecification id" );
+		writeln( " --specs:   \tlist specification id" );
+		writeln( " --verbose -v:\tenable verbosity" );
 		writeln( "" );
 		writeln( "Author:" );
-		writeln( "Marc-Antoine ARNAUD - arnaud.marcantoine@gmail.com" );
+		writeln( " Marc-Antoine ARNAUD - arnaud.marcantoine@gmail.com" );
 		return;
 	}
 
-	if( filename.length == 0 )
-	{
+	if( specs ){
+		ListSpecifications l = new ListSpecifications(true);
+		foreach( Specification s; l.getAllSpecs() ){
+			writeln( s._id, "\t", s._label );
+		}
+		return;
+	}
+
+	if( filename.length == 0 ){
 		writeln( "meta require an input filename." );
 		return;
 	}
 
+	ListSpecifications l = new ListSpecifications();
+	l.load();
+
+	if( ! l.contains( spec ) ){
+		writeln( "specification was not recognize." );
+		return;
+	}
+
+	auto s = l.get( spec );
+
 	writeln( "start extract meta" );
 
-	//auto s = new WavSpecification();
-	auto s = new MxfSpecification();
 	auto c = new Comparator( s, filename );
 
 	c.run();

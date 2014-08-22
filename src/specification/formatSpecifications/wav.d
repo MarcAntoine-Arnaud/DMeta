c81c9f534a29af7cfba418ccd66e2bcd47cfa230
marco <arnaud.marcantoine@gmail.com>
2014-08-22 22:54:45 +0200

    update wav specification

--- src/specification/formatSpecifications/wav.d
+++ src/specification/formatSpecifications/wav.d
@@ -1,13 +1,14 @@
 import specification.spec;
 import specification.node;
 import specification.nodes.nodeGroup;
+import specification.nodes.nodeString;
 import specification.nodes.nodeChar;
 import specification.nodes.nodeShort;
 import specification.nodes.nodeInt;
 
 class WavSpecification : Specification{
 	string rootId = "root";
-	string chunkRiffId = "chunckRIFF";
+	string chunkMagickId = "magickChunck";
 	string chunkWaveId = "chunckWAVE";
 	string chunkSubRiffId = "subRIFF";
 	string chunkSizeId = "chunckSize";
@@ -20,7 +21,6 @@ class WavSpecification : Specification{
 	string blockAlignmentId = "blockAlignment";
 	string bitsPerSampleId = "bitsPerSample";
 
-	
 	this() {
 		_id = "wav";
 		_label = "Waveform Audio File Format";
@@ -28,25 +28,11 @@ class WavSpecification : Specification{
 		_extensions =  [ "wav", "wave" ];
 		
 		auto root = new NodeGroup( rootId );
-		auto riffChunk = new NodeGroup( chunkRiffId );
-		auto waveChunk = new NodeGroup( chunkWaveId );
-		auto fmtChunk = new NodeGroup( chunkFmtId );
-
-		auto rChunk = new NodeChar( chunkSubRiffId );
-		auto iChunk = new NodeChar( chunkSubRiffId );
-		auto fChunk = new NodeChar( chunkSubRiffId );
+		auto magickChunk = new NodeString( chunkMagickId );
+		auto waveChunk = new NodeString( chunkWaveId );
+		auto fmtChunk = new NodeString( chunkFmtId );
 		
 		auto chunkSize = new NodeInt( chunkSizeId );
-
-		auto wRiffType = new NodeChar( riffTypeId );
-		auto aRiffType = new NodeChar( riffTypeId );
-		auto vRiffType = new NodeChar( riffTypeId );
-		auto eRiffType = new NodeChar( riffTypeId );
-
-		auto f = new NodeChar( chunkFmtId );
-		auto m = new NodeChar( chunkFmtId );
-		auto t = new NodeChar( chunkFmtId );
-		auto space = new NodeChar( chunkFmtId );
 
 		auto fmtType = new NodeShort( fmtTypeId );
 		auto channelCount = new NodeShort( channelCountId );
@@ -55,35 +41,9 @@ class WavSpecification : Specification{
 		auto blockAlignment = new NodeShort( blockAlignmentId );
 		auto bitsPerSample = new NodeShort( bitsPerSampleId );
 		
-
-		rChunk.setValue( 'R' );
-		iChunk.setValue( 'I' );
-		fChunk.setValue( 'F' );
-
-		wRiffType.setValue( 'W' );
-		aRiffType.setValue( 'A' );
-		vRiffType.setValue( 'V' );
-		eRiffType.setValue( 'E' );
-
-		f.setValue( 'f' );
-		m.setValue( 'm' );
-		t.setValue( 't' );
-		space.setValue( ' ' );
-		
-		riffChunk.addChild( rChunk );
-		riffChunk.addChild( iChunk );
-		riffChunk.addChild( fChunk );
-		riffChunk.addChild( fChunk );
-
-		waveChunk.addChild( wRiffType );
-		waveChunk.addChild( aRiffType );
-		waveChunk.addChild( vRiffType );
-		waveChunk.addChild( eRiffType );
-
-		fmtChunk.addChild( f );
-		fmtChunk.addChild( m );
-		fmtChunk.addChild( t );
-		fmtChunk.addChild( space );
+		magickChunk.setValue( "RIFF" );
+		waveChunk.setValue( "WAVE" );
+		fmtChunk.setValue( "fmt " );
 
 		chunkSize.setEndianness(EndiannessType.eLittle);
 		channelCount.setEndianness(EndiannessType.eLittle);
@@ -100,7 +60,7 @@ class WavSpecification : Specification{
 		fmtChunk.addChild( blockAlignment );
 		fmtChunk.addChild( bitsPerSample );
 
-		root.addChild( riffChunk );
+		root.addChild( magickChunk );
 		root.addChild( chunkSize );
 		root.addChild( waveChunk );
 		root.addChild( fmtChunk );
